83b5d46d8b48d0261647e3070b200b445b8dd2aa
Tomas Bubenicek <tom@tombuben.eu>
2026-01-12 13:32:17 +0100

    Added example of how I expect the whole dialogue to be defined for an entire day

--- Scripts/global_manager.gd
+++ Scripts/global_manager.gd
@@ -8,3 +8,12 @@ var world_view : WorldView
 var rotate_around : RotateAround
 
 var screen_resizer : ScreenResizer
+
+
+signal play_animation(animation_name : String)
+var played_animation_duration : float
+
+signal person_shoot_selection_started(person_name : String)
+var person_hit_with_item : String
+
+signal recieve_item(item : String)
