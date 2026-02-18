6712bb57a6bb063f766d492b9a6522dfa3dc7843
Tomas Bubenicek <tom@tombuben.eu>
2026-02-18 22:28:21 +0100

    remove process from global animation player

--- Scripts/global_animation_player.gd
+++ Scripts/global_animation_player.gd
@@ -5,11 +5,6 @@ extends AnimationPlayer
 func _ready() -> void:
 	GlobalManager.play_animation.connect(play_animation)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func play_animation(animation_name : String):
 	if not has_animation(animation_name):
 		print("Animation " + animation_name + " is missing!")
