037910eba6bb2e685a0535b88f187c1a144c9be8
Tomas Bubenicek <tom@tombuben.eu>
2026-06-07 20:01:40 +0200

    Hide cursor when conversation open

--- Scripts/mouse_cursor.gd
+++ Scripts/mouse_cursor.gd
@@ -2,6 +2,16 @@ class_name MouseCursor extends Node2D
 
 var rotation_target : float
 
+func _ready() -> void:
+	DialogueManager.dialogue_started.connect(on_dialogue_started)
+	DialogueManager.dialogue_ended.connect(on_dialogue_closed)
+
 func _process(delta: float) -> void:
 	position = get_viewport().get_mouse_position()
 	return
+
+func on_dialogue_started(resource: Resource) -> void:
+	visible = false
+
+func on_dialogue_closed(resource: Resource) -> void:
+	visible = true
