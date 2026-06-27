b1967ba7841ac1c795ddfa72030278e68e81ef87
Matěj Hes <hesmatej1@gmail.com>
2026-06-27 15:21:37 +0200

    dialogue beeps update

--- Scripts/dialogue_audio_beeps.gd
+++ Scripts/dialogue_audio_beeps.gd
@@ -1,8 +1,8 @@
 extends AudioStreamPlayer
 
-@export var audio_samples : Array[AudioStream]
+@export var audio_samples: Array[AudioStream]
+@export var letters_per_beep := 2
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	%DialogueLabel.spoke.connect(on_spoke)
 	GlobalManager.update_audio_beeps.connect(update_audio_beeps)
@@ -11,6 +11,13 @@ func update_audio_beeps(new_beep_array: Array[AudioStream]):
 	audio_samples = new_beep_array
 
 func on_spoke(letter: String, letter_index: int, speed: float) -> void:
-	if not playing:
-		stream = audio_samples.pick_random()
-		play()
+	# Ignore spaces and punctuation.
+	if letter in [" ", ".", ",", "!", "?", ":", ";"]:
+		return
+
+	# Only beep every N letters.
+	if letter_index % letters_per_beep != 0:
+		return
+
+	stream = audio_samples.pick_random()
+	play()
